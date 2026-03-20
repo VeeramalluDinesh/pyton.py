dc1784dfa521e9fe6ed018bbb380a2d352d89128
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-03-20 22:26:30 +0530

    variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+#variables
+x=5
+y="hello"
+X=10
+print(type(x),type(y))
+print(x,X) #case sensitive
+a,b,c="bike","car","scooty"
+print(x)
+print(y,z)
+print(x+y+z) #use + for multiple operators
+print(x+X) #mathematical operator
+
