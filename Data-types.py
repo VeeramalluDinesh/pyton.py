ee4fb55e23077b3be30518bd753267aebd7d6d91
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-03-22 20:49:07 +0530

    Data-types.py

---
+++ Data-types.py
@@ -0,0 +1,15 @@
+#Data types
+x=34
+y=2.0
+z="Dinesh"
+a=5+6j
+b=["Apple","Banana']
+c=("onion","Potato")
+d={"name":"Dinesh","Age":20)
+print(type(x),type(y),type(z))
+print(type(a),type(b),type(c),type(d))
+# Type casting
+x=int(4.9)
+y=float(87)
+z=str(2)
+print(x,y,z)
