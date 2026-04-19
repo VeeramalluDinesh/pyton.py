a2883d3128d6030e4b8af969ea8c922fdd8c3824
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-04-19 12:19:43 +0530

    Create Traverse_dict.py

---
+++ Traverse_dict.py
@@ -0,0 +1,15 @@
+def traverse_dict(data, level=0):
+    for key, value in data.items():
+        if isinstance(value, dict):
+            print("  " * level + f"{key}:")
+            traverse_dict(value, level + 1)
+        else:
+            print("  " * level + f"{key}: {value}"
+
+data = {
+    "a": 1,
+    "b": {"c": 2, "d": {"e": 3}},
+    "f": 4
+}
+
+traverse_dict(data)
