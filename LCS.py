c5ea8ff3e35f835d9c070649e16a0556f9e746fe
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-04-19 12:18:47 +0530

    Create LCS.py

---
+++ LCS.py
@@ -0,0 +1,26 @@
+def longest_common_substring(str1, str2):
+    m, n = len(str1), len(str2)
+    
+    # Create DP table
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    max_length = 0
+    end_index = 0
+
+    # Fill the table
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+                
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+                    end_index = i
+            else:
+                dp[i][j] = 0
+
+    return str1[end_index - max_length:end_index]
+
+
+# Example
+lcs = longest_common_substring("ABCDE", "BCDFG")
+print("LCS:", lcs)
